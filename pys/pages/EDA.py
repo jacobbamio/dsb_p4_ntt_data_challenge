0dc2fa14f4e513f0c72efa9d693be9a5dfc03f35
ignaciopalmero <ignciopm@gmail.com>
2023-03-27 21:38:23 +0200

    Streamlit structure

---
+++ pys/pages/EDA.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+import os
+
+
+
+st.set_page_config(page_title="EDA", page_icon="💰")
+
+
+
+st.title("Fixed deposit Predictor")
